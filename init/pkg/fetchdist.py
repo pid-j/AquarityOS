b9c024843d41e6a965946c4806dcc51134184732
Pigeon <146737777+pid-j@users.noreply.github.com>
2024-05-03 21:15:54 +0300

    Removed user display in fetchdist

--- init/pkg/fetchdist.py
+++ init/pkg/fetchdist.py
@@ -12,7 +12,7 @@ uname = platform.uname()
 
 def Main(args):
     return """
-\x1b[38;5;27m     ((((            ((/((           (((,           .(((     \x1b[0m  %s              
+\x1b[38;5;27m     ((((            ((/((           (((,           .(((     \x1b[0m  fetchdist             
 \x1b[38;5;27m  ((      ((      /((     ((       ((    ((       ((    ((   \x1b[0m  -------------------
 \x1b[38;5;27m((          ((/ ((           (( ((          (( ((          ((\x1b[0m  %s              
 \x1b[38;5;27m       (              /              *                       \x1b[0m  %s              
@@ -20,7 +20,6 @@ def Main(args):
 \x1b[38;5;27m  ((       ((    ((,      ((/   ((,      ((/   /((      (((  \x1b[0m  %s              
 \x1b[38;5;27m((            (((            (((            (((            ((\x1b[0m  %s    
 """ % (
-        "root@aquarity",
         f"\x1b[38;5;27mOS\x1b[0m: Aquarity v1.0a LTS {sysinfo['architecture']}",
         f"\x1b[38;5;27mHost\x1b[0m: {uname.node} on {sysinfo['os']}",
         "\x1b[38;5;27mShell\x1b[0m: aqx shell",
