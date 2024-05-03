1861962a29ee1b29434df2de61923a81d6327dea
Pigeon <146737777+pid-j@users.noreply.github.com>
2024-05-03 22:14:18 +0300

    Update fetchdist.py

--- init/pkg/fetchdist.py
+++ init/pkg/fetchdist.py
@@ -20,7 +20,7 @@ def Main(args):
 \x1b[38;5;27m  ((       ((    ((,      ((/   ((,      ((/   /((      (((  \x1b[0m  %s              
 \x1b[38;5;27m((            (((            (((            (((            ((\x1b[0m  %s    
 """ % (
-        f"\x1b[38;5;27mOS\x1b[0m: Aquarity v1.0 LTS {sysinfo['architecture']}",
+        f"\x1b[38;5;27mOS\x1b[0m: Aquarity v1.0beta LTS {sysinfo['architecture']}",
         f"\x1b[38;5;27mHost\x1b[0m: {uname.node} on {sysinfo['os']}",
         "\x1b[38;5;27mShell\x1b[0m: aqx shell",
         "\x1b[38;5;27mDesktop\x1b[0m: No aquarity GUI",
